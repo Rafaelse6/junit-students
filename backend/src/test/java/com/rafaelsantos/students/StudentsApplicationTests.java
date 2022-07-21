4ac9f493dd2081b7a5fed07ff771eab4992a41b7
Rafael Santos <rafa.05125@gmail.com>
2022-07-21 16:58:41 -0300

    Main class tested

--- backend/src/test/java/com/rafaelsantos/students/StudentsApplicationTests.java
+++ backend/src/test/java/com/rafaelsantos/students/StudentsApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class StudentsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		StudentsApplication.main(new String[] {});
 	}
 
 }
