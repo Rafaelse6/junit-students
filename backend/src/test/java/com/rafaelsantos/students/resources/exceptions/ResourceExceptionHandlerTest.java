dc34c6621f5a6a8d16c5d274894a95dfc40d543d
Rafael Santos <rafa.05125@gmail.com>
2022-09-06 09:06:29 -0300

    DataIntegrityViolaionException in ResourceExceptionHandler tested

--- backend/src/test/java/com/rafaelsantos/students/resources/exceptions/ResourceExceptionHandlerTest.java
+++ backend/src/test/java/com/rafaelsantos/students/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.rafaelsantos.students.resources.exceptions;
 
+import com.rafaelsantos.students.services.exceptions.DataIntegrityViolationException;
 import com.rafaelsantos.students.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     private static final String OBJECT_NOT_FOUND = "Object not found";
+
+    private static final String E_MAIL_ALREADY_IN_USE = "E-mail already in  use";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -38,9 +43,23 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJECT_NOT_FOUND, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/student/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
     void dataIntegrityViolationException() {
+
+        ResponseEntity<StandardError> response = exceptionHandler.
+                dataIntegrityViolationException(new DataIntegrityViolationException(E_MAIL_ALREADY_IN_USE),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_ALREADY_IN_USE, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
